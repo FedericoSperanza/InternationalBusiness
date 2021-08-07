d76ce9fabaf37beb55821c2bba889a4f18a1e72b
Federico Speranza <fedde.speranza@hotmail.com>
2021-08-07 16:18:09 -0300

    Currency and EndPoints Data models

---
+++ InternationalBusiness.Core/Models/EndPoints.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternationalBusiness.Core.Models
+{
+    public class EndPoints
+    {
+        public string CurrenciesAPI { get; set; }
+        public string TransactionsAPI { get; set; }
+    }
+}
