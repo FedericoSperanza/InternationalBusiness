0e0afd6649d64d5053aacb560fc70a33908051df
Federico Speranza <fedde.speranza@hotmail.com>
2021-08-08 18:09:03 -0300

    Transaction model added

---
+++ InternationalBusiness.Core/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternationalBusiness.Core.Models
+{
+    public class Transaction
+    {
+        public string sku { get; set; }
+        public double amount { get; set; }
+        public string currency { get; set; }
+    }
+}
