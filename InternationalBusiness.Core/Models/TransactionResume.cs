a157b25a0efb6f325fd05bb6eab5e1df842d9fc2
Federico Speranza <fedde.speranza@hotmail.com>
2021-08-08 18:09:10 -0300

    TransactionResume model added

---
+++ InternationalBusiness.Core/Models/TransactionResume.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternationalBusiness.Core.Models
+{
+    public class TransactionResume
+    {
+        public List<Transaction> transactionList {get; set;}
+        public int totalAmount { get; set; }
+    }
+}
